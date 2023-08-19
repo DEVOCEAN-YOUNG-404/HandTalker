ec11060c3267f5974af034b57b2e8d0bfd4487f0
SEUNGJUN JEONG <jsj9620@hufs.ac.kr>
2023-08-19 14:12:53 +0900

    Feat: Signup form error validation

--- front/src/pages/Auth/SIgnUp/SignUpForm.tsx
+++ front/src/pages/Auth/SIgnUp/SignUpForm.tsx
@@ -1,26 +1,27 @@
-import { Link } from "react-router-dom";
 import { useForm, Controller } from "react-hook-form";
 
 interface FormData {
   email: string;
   password: string;
+  confirmPassword: string;
 }
 
-const LoginForm = () => {
+const SignUpForm = () => {
   const {
     control,
     handleSubmit,
     formState: { errors },
+    getValues,
   } = useForm<FormData>();
 
-  const loginHandler = (data: FormData) => {
+  const signupHandler = (data: FormData) => {
     console.log(data.email, data.password);
   };
 
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
-      <p className="text-5xl font-bold font-main mb-[50px]">로그인</p>
-      <form onSubmit={handleSubmit(loginHandler)} className="w-[310px]">
+      <p className="text-5xl font-bold font-main mb-[50px]">회원가입</p>
+      <form onSubmit={handleSubmit(signupHandler)} className="w-[310px]">
         <div className="mb-3 outline-none">
           <Controller
             name="email"
@@ -101,23 +102,54 @@ const LoginForm = () => {
             {errors.password?.message}
           </p>
         </div>
+        <div className="mb-3 outline-none">
+          <Controller
+            name="confirmPassword"
+            control={control}
+            rules={{
+              required: "비밀번호를 다시 입력해주세요",
+              validate: (value) =>
+                value === getValues("password") ||
+                "비밀번호가 일치하지 않습니다",
+            }}
+            render={({ field }) => (
+              <div
+                className={`outline mb-3 ${
+                  errors.confirmPassword ? "border-red-500" : ""
+                } flex items-center relative w-[310px] h-[48px] transition-colors duration-300 border border-gray-300 px-2 rounded-lg outline-none`}
+              >
+                <input
+                  type="password"
+                  {...field}
+                  placeholder=" "
+                  className="block w-full h-full bg-transparent appearance-none focus:outline-none"
+                />
+                <label
+                  htmlFor="confirmPassword"
+                  className="absolute duration-300 bg-white origin-0 text-start -z-10"
+                >
+                  비밀번호 재입력
+                </label>
+              </div>
+            )}
+          />
+          <p
+            className={`${
+              errors.confirmPassword ? "mt-[-7px]" : ""
+            } text-xs text-red-500 font-main`}
+          >
+            {errors.confirmPassword?.message}
+          </p>
+        </div>
         <button
           type="submit"
           className="w-[310px] h-[48px] bg-main-2 rounded-lg font-main font-bold text-lg text-white transition-colors duration-150 hover:bg-green-600"
         >
-          로그인
+          회원가입
         </button>
       </form>
-      <div className="flex flex-row mt-[20px]">
-        <p className="text-gray-400 text-md font-main ">계정이 없으신가요?</p>
-        <Link to="/signup">
-          <p className="ml-1 font-semibold text-main-2 text-md font-main">
-            회원가입 하기
-          </p>
-        </Link>
-      </div>
     </div>
   );
 };
 
-export default LoginForm;
+export default SignUpForm;
