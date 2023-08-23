7c4797ac1918a3385607dfe8ce30b02debb2a0b8
SEUNGJUN JEONG <jsj9620@hufs.ac.kr>
2023-08-23 23:06:16 +0900

    Design: 여러가지 수정 사항/오류 반영

--- front/src/pages/PlugIn/modal/MPage1.tsx
+++ front/src/pages/PlugIn/modal/MPage1.tsx
@@ -3,7 +3,7 @@ import discord from "../../../assets/images/discord_screenshot.png";
 const MPage1 = () => {
   return (
     <div className="flex flex-col items-center justify-center w-[730px] h-[520px]">
-      <img src={discord} alt="디스코드 화면" className="w-[500px]" />
+      <img src={discord} alt="디스코드 화면" className="w-auto h-[430px]" />
       <p className="text-xl font-main">
         1. 디스코드 데스크톱 애플리케이션을 실행해주세요.
       </p>
--- front/src/pages/TranslatePage/config/ConfigSuccess.tsx
+++ front/src/pages/TranslatePage/config/ConfigSuccess.tsx
@@ -1,4 +1,18 @@
-export const ConfigSuccess = () => {
+import { useEffect } from "react";
+
+interface ConfigSuccessProps {
+  onModalAlert: () => void;
+}
+
+export const ConfigSuccess = ({ onModalAlert }: ConfigSuccessProps) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onModalAlert();
+    }, 1500);
+    return () => clearTimeout(timer);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
       <p className="mb-8 text-7xl">🥳</p>
